4b8353bac8f63ca07701ea7b2ac50fe7eff2d3b6
Tobias Hultqvist <hultqvist.tobias@gmail.com>
2022-05-15 18:21:52 +0200

    Added migrations.sql

---
+++ migrations.sql
@@ -0,0 +1,66 @@
+﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
+    `ProductVersion` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+) CHARACTER SET=utf8mb4;
+
+START TRANSACTION;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20220514204845_CreateNews') THEN
+
+    ALTER DATABASE CHARACTER SET utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20220514204845_CreateNews') THEN
+
+    CREATE TABLE `News` (
+        `Id` bigint NOT NULL AUTO_INCREMENT,
+        `Status` int NOT NULL,
+        `PublishedAt` datetime(6) NULL,
+        `Title` longtext CHARACTER SET utf8mb4 NULL,
+        `Image` longtext CHARACTER SET utf8mb4 NULL,
+        `Content` longtext CHARACTER SET utf8mb4 NULL,
+        `Featured` tinyint(1) NOT NULL,
+        `Category` int NOT NULL,
+        `AuthorId` bigint NULL,
+        `CreatedAt` datetime(6) NULL,
+        `UpdatedAt` datetime(6) NULL,
+        CONSTRAINT `PK_News` PRIMARY KEY (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20220514204845_CreateNews') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20220514204845_CreateNews', '6.0.5');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+COMMIT;
+
